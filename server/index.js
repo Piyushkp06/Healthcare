964881f6dae3bd05e672b69974331908a4d7ff26
Piyushkp06 <pkpanda0612@gmail.com>
2025-06-19 16:35:33 +0530

    made cors changes

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.ORIGIN || "http://localhost:5173",
+    origin: process.env.ORIGIN ,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
