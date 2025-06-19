08c72576322e7452fd2eed2fc0d2b47a599bfd43
Piyushkp06 <pkpanda0612@gmail.com>
2025-06-19 15:39:01 +0530

    changed a path

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Home from "../src/pages/Home";
+import Home from "./pages/Home";
 import Login from "./pages/Auth/Login";
 import AdminDashboard from "./pages/AdminDash/Dashboard";
 import DoctorDashboard from "./pages/Doctor/Dashboard";
-import FrontdeskPage from "../src/pages/Frontdesk/Index";
+import FrontdeskPage from "./pages/Frontdesk/Index";
 import AllDoctorsPage from "./pages/Doctors/Index";
 
 export const router = createBrowserRouter([
